e11feab94516880c17068837180605915c152798
Jafaru Idris <jafaridris82@gmail.com>
2024-09-15 21:55:10 +0100

    do mini implemeny

--- core/contexts.py
+++ core/contexts.py
@@ -1,8 +1,9 @@
  
 INFO = {
-  "pr_long_name": "National Association of Political Science Student & African Society for International Studies",
-  "pr_short_name": "NAPSS & ASIS Blpgspot",
+  "pr_long_name": "National Association of Political Science Student & African Society of International Studies E-Library",
+  "pr_short_name": "NAPSS & ASIS E-Library",
   "pr_small_logo": "imgs/small_logo.jpg" ,
+  "pr_napss_logo": "imgs/napss.jpg" ,
   "pr_big_logo": "",
   "pr_allowed_apps": ['admin', 'app', 'api', 'material'],
   "pr_app_maintenance": [],
