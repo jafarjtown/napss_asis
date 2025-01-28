e11b8c9fb324c9da04c0bf47b44699f25ce5872f
Jafaru <jafaridris82@gmail.com>
2025-01-28 02:28:43 +0100

    update cbt

--- core/contexts.py
+++ core/contexts.py
@@ -5,9 +5,9 @@ INFO = {
   "pr_small_logo": "imgs/logo.png" ,
   "pr_napss_logo": "imgs/tr_logo.png" ,
   "pr_big_logo": "",
-  "pr_allowed_apps": ['admin', 'app', 'api', 'material'],
+  "pr_allowed_apps": ['admin', 'app', 'api', 'material', 'cbt'],
   "pr_allowed_views": ['wallet:generated_link'],
-  "pr_app_maintenance": ['service', 'wallet', 'blog', 'cbt'],
+  "pr_app_maintenance": ['service', 'wallet', 'blog',],
   "pr_view_maintenance": ['material:timetables'],
   "pr_only_staff_views": ['blog:write'],
 }
